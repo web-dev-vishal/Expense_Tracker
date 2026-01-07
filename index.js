f68adfa1cb86dc59dd3d01647c4d0b8f08d5cbe4
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 00:47:28 +0530

    basic setup done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors("*"))
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ extended: true }))
