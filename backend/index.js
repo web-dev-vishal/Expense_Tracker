65003ed7083d0d04c44958ce5b38b3ecf9ee2b6b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 07:37:17 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
 
+import router from "./routes/index.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 8000;
 app.use(cors())
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
+
+app.use("/api-v1",router);
 
 app.use("/",(req,res) => {
     res.json({message:"Expense Tracker is Up and Runnig"});
