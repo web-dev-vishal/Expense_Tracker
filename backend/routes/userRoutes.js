10a36d2ff20deb92545df3d786d0cbb0715efa5a
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 19:49:07 +0530

    auth contoller auth router user router

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+// import all controllers
+// import SessionController from './app/controllers/SessionController';
+
+// Add routes
+// routes.get('/', SessionController.store);
+// routes.post('/', SessionController.store);
+// routes.put('/', SessionController.store);
+// routes.delete('/', SessionController.store);
+
+export default router;
