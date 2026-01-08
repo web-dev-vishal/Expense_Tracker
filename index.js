579b61f68388f268be4d8e09020bc1e1510186bd
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 07:26:14 +0530

    commit

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ app.use(cors())
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
 
-app.use("/", (req, res) => {
-  res.status(404).json({
-    status: "404 Not Found",
-    message: "Route not found"
-  });
-});
+app.use("/",(req,res) => {
+    res.json({message:"Expense Tracker is Up and Runnig"});
+})
 
 
 app.listen(PORT, ()=> {
