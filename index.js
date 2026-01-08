8f12b6b474c9330234eeae43c3819a26e282b295
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 07:16:58 +0530

    commit

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors("*"))
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api-v1',router);
 
 app.use("*", (req,res)=>{
     res.status(404).json({
