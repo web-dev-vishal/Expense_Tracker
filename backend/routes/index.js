3067cf0f8a1fd651df3eede60d9ee38955dbc5ff
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 07:43:22 +0530

    commit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,10 @@
 import express from "express";
 
+import authRoutes from "./authRoutes.js";
+import accountRoutes from "./accountRoutes.js";
+import transactionRoutes from "./transactionRoutes.js";
+import userRoutes from "./authRoutes.js";
+
 const router  = express.Router();
 
 
