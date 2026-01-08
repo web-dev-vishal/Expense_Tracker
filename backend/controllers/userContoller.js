698c61bea2c4b1ccec7bec9db575d0c3da5f3522
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-09 00:02:43 +0530

    userContoller change password done

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -24,14 +24,6 @@ export const getUser = async (req, res) => {
 
 export const changePassword = async (req, res) => {
     try {
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).res.json({ status: "Failed", message: error.message });
-    }
-}
-export const updateUser = async (req, res) => {
-    try {
         const { userId } = req.body.user;
 
         const { currentPassword, newPassword, confifPassword } = req.body;
@@ -44,12 +36,40 @@ export const updateUser = async (req, res) => {
 
         if (!user) { return res.status(404).json({ status: "Failed", message: "User not found" }) };
 
+        if (newPassword !== confifPassword) {
+            return res.status(401).json({ status: "failed", message: "New Password does not match" })
+        }
+
+        const isMatch = await comparePassword(currentPassword, user?.password);
+
+        if (!isMatch) { return res.status(401).json({ status: "failed", message: "Invalid current password" }) }
+
+        const hashedPassword = await hashedPassword(newPassword)
+
+        await pool.query({
+            text: `UPDATE SET password = $1 WHERE id = $2`,
+            values: [hashedPassword, userId],
+        });
+
+        res.status(200).json({
+            status: "success",
+            message: "Password changed successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).res.json({ status: "Failed", message: error.message });
+    }
+}
+export const updateUser = async (req, res) => {
+    try {
 
     } catch (error) {
         console.log(error);
         res.status(500).res.json({ status: "Failed", message: error.message });
     }
 }
+
+
 // export const updateUser = async (req, res) => {
 //     try {
 
