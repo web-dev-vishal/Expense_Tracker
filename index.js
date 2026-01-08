c17128fa932509769fafd953d08cec07a1ab5e30
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 07:20:45 +0530

    commit

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors("*"))
+app.use(cors())
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
 
-
-app.use("*", (req,res)=>{
-    res.status(404).json({
-        status: "404 Not Found",
-        message: 'Route not found'
-    })
+app.use("/", (req, res) => {
+  res.status(404).json({
+    status: "404 Not Found",
+    message: "Route not found"
+  });
 });
+
 
 app.listen(PORT, ()=> {
     console.log(`Server is listening at http://localhost:{PORT}`)
