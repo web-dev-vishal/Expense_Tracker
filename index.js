5e88243d22527107d46f56d6c37d185349dbd36b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 07:13:01 +0530

    basic setup done

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ const PORT = process.env.PORT || 8000;
 app.use(cors("*"))
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/api-v1',router);
+
+app.use("*", (req,res)=>{
+    res.status(404).json({
+        status: "404 Not Found",
+        message: 'Route not found'
+    })
+});
