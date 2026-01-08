c4640b810d63c3540347fa2b27cb895a0a971822
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 23:26:37 +0530

    userContoller

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -1,6 +1,12 @@
 export const getUser = async (req, res) => {
-    
-}
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).res.json({ status: "Failed", message: error.message});
+    }
+};
+
 export const changePassword = async (req, res) => {
 
 }
