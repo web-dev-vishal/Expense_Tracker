b45b14772ed6c3691773338e5c58f6ce2c347e29
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 23:34:58 +0530

    userContoller

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -1,6 +1,11 @@
 export const getUser = async (req, res) => {
     try {
-        
+        const { userId } = req.body.user;
+
+          const userExist = await Pool.query({
+            text: `SELECT * FROM tbluser WHERE id = $1`,
+            values: [userId],
+          });
     } catch (error) {
         console.log(error);
         res.status(500).res.json({ status: "Failed", message: error.message});
