7e35bdc519ffb3c593c7f140a56295da636427e9
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-10 23:01:46 +0530

    User Register Done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,48 @@
+const User = require('../models/User.js');
 const jwt = require("jsonwebtoken");
 
 // Generate JWT token
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '1h'});
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 }
 
 // Register User
-exports.registerUser = async (req,res) => {};
+exports.registerUser = async (req, res) => {
+
+    const { fullName, email, password, profileImageUrl } = req.body;
+
+    // Validation: Check for missing fields
+    if (!fullName || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        // Check if email alread exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already in use try different Email" });
+        }
+
+        //Creating a user
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl
+        });
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch (err) {
+        res.status(500).json({ message: "Error registering user", error: err.message });
+    }
+};
 
 // Login User
-exports.loginUser = async (req,res) => {};
+exports.loginUser = async (req, res) => { };
 
 // GetInfo User
-exports.getUserInfo = async (req,res) => {};
+exports.getUserInfo = async (req, res) => { };
