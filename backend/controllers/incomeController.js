8b4c3d4edeafd2b63efbf212ea88ddd6bf6964c5
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-11 08:21:38 +0530

    Income Contoller setup Done

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -4,19 +4,19 @@ const jwt = require("jsonwebtoken");
 // Add Income Source
 exports.addIncome = async (req, res) => {
 
-}
+};
 
 // Get ALL income
 exports.getAllIncome = async (req, res) => {
 
-}
+};
 
 // Add Income Source
 exports.deleteIncome = async (req, res) => {
 
-}
+};
 
 // Download Excel Sheet
 exports.downloadIncomeExcel = async (req, res) => {
 
-}
+};
