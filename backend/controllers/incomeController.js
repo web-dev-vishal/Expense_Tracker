47b71d2cbb0ee430ebf54b35351483a98bb9ade7
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-11 08:32:22 +0530

    Add income Done

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -3,7 +3,29 @@ const jwt = require("jsonwebtoken");
 
 // Add Income Source
 exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
 
+    try {
+        const { icon, source, amount, date } = req.body;
+
+        // validation Check for missing fields
+        if (!source || !amount || !date) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        });
+
+        await newIncome.save();
+        res.status(200).json(newIncome);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
 };
 
 // Get ALL income
