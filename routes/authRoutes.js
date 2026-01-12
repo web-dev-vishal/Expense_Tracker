dd6792ac635d05e1c8af690802ea467b9751bf28
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-12 20:32:09 +0530

    Update in Auth Middleaare & Routes Done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,31 +1,3 @@
-// const express = require("express");
-
-// // import all controller
-// const { registerUser, loginUser, getUserInfo } = require("../controllers/authController.js");
-
-// // import all middleware
-// const { protect } = require("../middleware/authMiddleware.js");
-// const upload = require("../middleware/uploadMiddleware.js");
-
-// // all routes
-// const router = express.Router();
-
-// router.post('/register',registerUser);
-
-// router.post('/login',loginUser);
-
-// router.get('/getUser', protect,getUserInfo);
-
-// router.post("/upload-image", upload.single("image"), (req, res) => {
-//   if (!req.file) {
-//     return res.status(400).json({ message: "No file uploaded" });
-//   }
-//   const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-//   res.status(200).json({ imageUrl });
-// });
-
-// module.exports = router;
-
 const express = require("express");
 
 // import all controller
