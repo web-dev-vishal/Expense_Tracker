30ea3f2162def0a7b93e076a5bfa80154f678411
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-12 12:18:11 +0530

    User Model update

--- models/User.js
+++ models/User.js
@@ -1,40 +1,3 @@
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
-
-// const UserSchema = new mongoose.Schema({
-//     fullName: {
-//         type: String,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     profileImageUrl: {
-//         type: String,
-//         default: null
-//     },
-// }, {timestamps: true});
-
-// // Hashing password before saving it
-// UserSchema.pre("save", async function (next) {
-//     if(!this.isModified("password")) return next();
-//     this.password = await bcrypt.hash(this.password, 10);
-//     next;
-// });
-
-// // Compare password
-// UserSchema.methods.comparePassword = async function (candidatePassword) {
-//     return await bcrypt.compare(candidatePassword, this.password);
-// };
-
-// module.exports = mongoose.model("User", UserSchema);
-
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
@@ -47,6 +10,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
+    },
+     phone: {
+        type: String,
+        default: null
     },
     password: {
         type: String,
